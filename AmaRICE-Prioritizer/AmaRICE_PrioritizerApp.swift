b2ea766f244b179b9fec9f3f1fb853d24cb1bf8b
Radcabna <radcabna@gmail.com>
2026-04-27 17:57:52 +0300

    Initial Commit

---
+++ AmaRICE-Prioritizer/AmaRICE_PrioritizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmaRICE_PrioritizerApp.swift
+//  AmaRICE-Prioritizer
+//
+//  Created by Алкександр Степанов on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AmaRICE_PrioritizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
